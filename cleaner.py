427dcffa4c168c66d1a0f0b582b80dbc9baf6775
ke <kxu1985@gmail.com>
2016-06-29 00:02:53 -0500

    add logging

--- cleaner.py
+++ cleaner.py
@@ -7,27 +7,31 @@ class Cleaner():
         
     def getList(self):
         """Get the full list of apt cache"""
-        logging.info('Getting the list...')
         self.apt_cache_path = '/var/cache/apt/archives/'
+        logging.info('Getting the list from %s...',self.apt_cache_path)
         self.pattern = '.deb'
         apt_deb_list = os.listdir(self.apt_cache_path)
         self.apt_deb_list = []
         for apt_deb in apt_deb_list:
-            #print("Check:" + apt_deb)
             if apt_deb.endswith(self.pattern):
                 self.apt_deb_list.append(apt_deb)
-        logging.info("\n".join(self.apt_deb_list))
+                logging.info('Got %s', apt_deb)
 
     def deleteFiles(self, file_path, file_list):
+        """Delete all the files in the list"""
+        logging.info('Removing the files from %s...',file_path)
         for self.file in file_list:
             os.remove(file_path+self.file)
+            logging.info('Removed %s', self.file)
 
 def main():
-    logging.basicConfig(filename='/var/log/ubuntu-cleaner.log', level=logging.DEBUG)
+    logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', \
+                        filename='/var/log/ubuntu-cleaner.log', \
+                        level=logging.DEBUG)
     cleaner = Cleaner()
     cleaner.getList()
     #print("\n".join(cleaner.apt_deb_list))
-    #cleaner.deleteFiles(cleaner.apt_cache_path, cleaner.apt_deb_list)
+    cleaner.deleteFiles(cleaner.apt_cache_path, cleaner.apt_deb_list)
     
 if __name__ == "__main__":
     main()
